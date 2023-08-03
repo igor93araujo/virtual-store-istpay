317c7b7ba84df92509e7d43e2091f37382cdc1b7
Igor <igoraraujobda@gmail.com>
2023-08-03 16:51:39 -0300

    productDetails-css

--- src/app/details/page.tsx
+++ src/app/details/page.tsx
@@ -4,13 +4,15 @@ import { AppContext } from '@/context/AppProvider';
 import Image from 'next/image';
 import React, { useContext, useEffect } from 'react'
 import { useRouter } from "next/navigation";
+import { AiOutlineRollback } from 'react-icons/ai';
+import './index.css'
 
 export default function Details() {
   
   const { push } = useRouter();
 
   const context = useContext(AppContext);
-  const { selectedProduct, setSelectedProduct, productsList, setCart, cart} = context || {};
+  const { selectedProduct, setSelectedProduct, productsList, setCart, cart, setCartCount} = context || {};
 
   const {id, title, price, description, category, image } = selectedProduct;
 
@@ -29,37 +31,43 @@ export default function Details() {
   const handleCart = (id: number) => {
     const productToAdd = productsList.find((product: any) => product.id === id);
     if (productToAdd) {
-      setCart((prevCart: any) => [...prevCart, productToAdd]);
+      setCart((prevCart) => [...prevCart, productToAdd]);
       const cartStorage = JSON.stringify([...cart, productToAdd]);
+      setCartCount((prevCount) => prevCount + 1);
       localStorage.setItem('cartStorage', cartStorage);
     }
   }
-
+  
   
   return (
     <section>
       <Header />
-      <button
-        type='button'
-        onClick={() => { push('/home')} }
-      >
-        Home
-      </button>
-      { selectedProduct ? (
-          <div style={{ display: 'block', width: 700, padding: 30 }}>
-            <h1>{title}</h1>
-            <Image src={image} alt='cartItem' width={200} height={200}/>
-            <p>{description}</p>
-            <p>{price}</p>
-            <p>{category}</p>
-            <button
-              type="button"
-              onClick={() => handleCart(id)}>
-                Add to cart
-            </button>
-          </div>
-      ) : <p>Nenhum produto selecionado</p>
-       }
+      <div className='getBack-container' onClick={() => { push('/home')}} >
+        <AiOutlineRollback />
+        <p>Voltar</p>
+      </div>
+      <div className='detail-container'>
+        <h1>Details</h1>
+        { selectedProduct ? (
+          <div className='detail-item-container-full'>
+            <h2>{title}</h2>
+          <div className='detail-item-container'>
+              <Image src={image} alt='cartItem' width={200} height={200}/>
+              <div className='detail-item-container-right'>
+                <h4>{description.charAt(0).toUpperCase() + description.slice(1)}</h4>
+                <p>{`Price: $${price}`}</p>
+                <span>{`Category: ${category}`}</span>
+                <button
+                  type="button"
+                  onClick={() => handleCart(id)}>
+                    Add to cart
+                </button>
+              </div>
+            </div>
+            </div>
+        ) : <p>Nenhum produto selecionado</p>
+        }
+       </div>
     </section>
   )
 }
